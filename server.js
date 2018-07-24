739610632bfdbec484ca35eae08895d337287bb7
rhoneybul <robert.honeybul@rthdatasolutions.com>
2018-07-24 21:12:41 +0800

    updated to add routes

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 
 var http = require('http');
 var fileSystem = require('fs');
+var express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./router");
+// var mongoose = require("mongoose");
 
-var server = http.createServer(function(req, resp){
-	fileSystem.readFile('./index.html', function(error, fileContent){
-		if(error){
-			resp.writeHead(500, {'Content-Type': 'text/plain'});
-			resp.end('Error');
-		}
-		else{
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.write(fileContent);
-			resp.end();
-		}
-	});
-});
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: "*/*" }))
+router(app)
 
-server.listen(8080);
+const port = 8080
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(`server listening on port: ${port}`)
 
 
